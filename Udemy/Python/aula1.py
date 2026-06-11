40d67a8df005ae5498db3bfbe9398b21f83b725e
Sofia Estrela Bernardes <bernardestrelasofia@hotmail.com>
2026-06-11 16:15:38 -0300

    Criação do arquivo aula1

---
+++ Udemy/Python/aula1.py
@@ -0,0 +1,12 @@
+# Um comentário permite escrever descrições a respeito de linhas
+# ou blocos de código
+
+
+"""
+Se chama DocString, permite escrever notas/comentários no código.
+"""
+
+"""
+Strings são textos dentro de aspas
+str -> string -> texto
+"""
